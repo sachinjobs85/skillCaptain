02dad9c4ca40af48a113067556b85f4e7a23461a
sachinjobs85 <121552849+sachinjobs85@users.noreply.github.com>
2024-10-02 15:43:04 +0530

    Create math_operations.py

---
+++ pythonPart3_Day2/math_operations.py
@@ -0,0 +1,19 @@
+def add(a, b):
+    c = a + b
+    return c
+
+def subtract(a, b):
+    c = a - b
+    return c
+
+def multiply(a, b):
+    c = a * b
+    return c
+
+def divide(a, b):
+    c = a / b
+    return c
+
+def power(a, b):
+    c = a ** b
+    return c
