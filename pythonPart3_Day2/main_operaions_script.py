eda5ea32281de7b43eda9d43330485402a79f187
sachinjobs85 <121552849+sachinjobs85@users.noreply.github.com>
2024-10-02 15:43:41 +0530

    Create main_operaions_script.py

---
+++ pythonPart3_Day2/main_operaions_script.py
@@ -0,0 +1,7 @@
+import math_operations
+
+print(math_operations.add(2, 3))
+print(math_operations.subtract(2, 3))
+print(math_operations.multiply(2, 3))
+print(math_operations.divide(2, 3))
+print(math_operations.power(2, 3))
